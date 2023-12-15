22b8dde374a55f35efa8ad85ac8afa254fc2d290
1N73RC3P70R <kras.konstantinov@yandex.ru>
2023-12-15 16:00:00 +0300

    ДЗ 1.1 - Done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,17 @@
 fun main() {
-    println("First App!")
+
+    val minimumRate: Int = 35;
+    val standardRate: Double = 0.0075;
+
+
+    println("Введите сумму: ")
+    var amount = readln().toDoubleOrNull();
+
+    val commissionTotal = if (amount!! <= minimumRate) {
+        println("$minimumRate  руб.")
+    } else {
+        println("${amount * standardRate}  руб.")
+    }
+
 
 }
